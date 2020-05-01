4f3926e25e384fb1b5b9ae629c450d78447e133d
hydeenoble <hydeenoble39@gmail.com>
2020-05-01 02:00:16 +0100

    added function to create a task

--- sheety/sheety.go
+++ sheety/sheety.go
@@ -2,7 +2,9 @@ package sheety
 
 import (
 	"bytes"
-	"encoding/json"
+	// "encoding/json"
+	// "github.com/tidwall/gjson"
+	"log"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -12,57 +14,30 @@ import (
 func CreateTask(tasks string){
 	
 	requestParam := fmt.Sprintf(`{
-		"tasks": {
-			"name": "%d",
+		"task": {
+			"name": "%v",
 			"age": "1122",
-			"sex": "male",
+			"sex": "male"
 		}
 	}`, tasks)
 
-	parsedRequestParam, _ := gjson.Parse(requestParam).Value().(map[string]interface{}) 
-
-	var parsedRequestParam = []byte()
-	// requestBody, err := json.Marshal(parsedRequestParam)
-// 	requestBody, err := json.Marshal(map[string]string{
-	// 	"tasks": {
-	// 		"name": tasks,
-	// 		"age": "1122",
-	// 		"sex": "male",
-	// 	},
-	// })
-
-	// if err != nil {
-    //     fmt.Print(err.Error())
-    //     os.Exit(1)
-    // }
-
-	req, err := http.NewRequest("POST",
-	"https://v2-api.sheety.co/8428d9964266c130d65343f01a3d5916/tctl/tasks", 
-	bytes.NewBuffer(parsedRequestParam))
+	response, err := http.Post("https://v2-api.sheety.co/8428d9964266c130d65343f01a3d5916/tctl/tasks",
+	"application/json", 
+	bytes.NewBuffer([]byte(requestParam)))
 	
-	client := &http.Client{}
-    resp, err := client.Do(req)
+
+	if err != nil {
+        fmt.Print(err.Error())
+        os.Exit(1)
+	}
+	
+	defer response.Body.Close()
+
+    responseData, err := ioutil.ReadAll(response.Body)
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
-    defer resp.Body.Close()
-
-    fmt.Println("response Status:", resp.Status)
-    body, _ := ioutil.ReadAll(resp.Body)
-    fmt.Println("response Body:", string(body))
-
-// 	if err != nil {
-//         fmt.Print(err.Error())
-//         os.Exit(1)
-// 	}
-	
-// 	defer response.Body.Close()
-
-//     responseData, err := ioutil.ReadAll(response.Body)
-//     if err != nil {
-//         log.Fatal(err)
-//     }
-//     fmt.Println(string(responseData))
+    fmt.Println(string(responseData))
 }
 
 func GetTasks(){
