96a903f3f9fe62f5aa1b8b051ca178f7930f771e
hydeenoble <hydeenoble39@gmail.com>
2020-05-01 07:58:25 +0100

    added the progress flag to the list command

--- cmd/list.go
+++ cmd/list.go
@@ -7,15 +7,24 @@ import (
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Run: func(cmd *cobra.Command, args []string) { 
-		p , _ := cmd.Flags().GetBool("pending")
+		b , _ := cmd.Flags().GetBool("backlog")
+		p, _ := cmd.Flags().GetBool("progress")
+		
 		if p {
-			sheety.GetTasks("pending");
+			sheety.GetTasks("progress");
 		}
+
+		if b {
+			sheety.GetTasks("backlog");
+		}
+
 		
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-	listCmd.Flags().BoolP("pending", "p", false, "Get only pending tasks")
+	listCmd.Flags().BoolP("backlog", "b", false, "Get only pending tasks")
+	listCmd.Flags().BoolP("progress", "p", false, "Get only tasks that are in progress")
+
 }
