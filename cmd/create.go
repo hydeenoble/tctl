17ceb27d8e6af6d27c068cd0de86cded12276066
hydeenoble <hydeenoble39@gmail.com>
2020-05-01 02:08:51 +0100

    removed comment and corrected function mapping to command

--- cmd/create.go
+++ cmd/create.go
@@ -9,7 +9,6 @@ var createCmd = &cobra.Command{
 	Use:   "create",
 	Run: func(cmd *cobra.Command, args []string) { 
 		fmt.Println("Create Command.")
-		// sheety.CreateTask("Work on helm");
-		sheety.GetTasks();
+		sheety.CreateTask("Work on helm");
 	},
 }
