44404eba8b91fa8ecce9f8aa03be35bb2bf283a9
hydeenoble <hydeenoble39@gmail.com>
2020-05-03 06:49:53 +0100

    added flag to get tasks that are done

--- cmd/list.go
+++ cmd/list.go
@@ -7,14 +7,17 @@ import (
 var listCmd = &cobra.Command{
 	Use:   "list",
 	Run: func(cmd *cobra.Command, args []string) { 
-		b , _ := cmd.Flags().GetBool("backlog")
+		b, _ := cmd.Flags().GetBool("backlog")
 		p, _ := cmd.Flags().GetBool("progress")
-		
+		d, _ := cmd.Flags().GetBool("done")
+
 		if p {
 			sheety.GetTasks("progress");
 		}
-
 		if b {
+			sheety.GetTasks("backlog");
+		}
+		if d {
 			sheety.GetTasks("backlog");
 		}
 
@@ -24,7 +27,7 @@ var listCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(listCmd)
-	listCmd.Flags().BoolP("backlog", "b", false, "Get only pending tasks")
-	listCmd.Flags().BoolP("progress", "p", false, "Get only tasks that are in progress")
-
+	listCmd.Flags().BoolP("backlog", "b", false, "Get only pending tasks.")
+	listCmd.Flags().BoolP("progress", "p", false, "Get only tasks that are in progress.")
+	listCmd.Flags().BoolP("done", "d", false, "Get only task that have been done.")
 }
